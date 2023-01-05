2c2f7cb50200f838a783cd5f692a53eb8ddf4c10
Luis Sanchez <luisanchezdh@gmail.com>
2023-01-05 18:38:13 -0500

    GET/POST requests

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
-const projectData = {
+let projectData = {
 }
 
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
+
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -16,6 +17,14 @@ const port = 8000;
 app.get('/all', function (req, res) {
   res.send(projectData);
 });
+
+app.post('/add', async (req, res) => {
+  const body = await req.body;
+  projectData = body;
+  console.log(projectData);
+  res.status(200).send(projectData);
+});
+
 
 app.use(express.static("website"));
 const server = app.listen(port, () => { console.log(`running on localhost: ${port}`) })
--- website/app.js
+++ website/app.js
@@ -1,22 +1,85 @@
+let d = new Date();
+let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
+
 const baseURL = ('https://api.openweathermap.org/data/2.5/weather?zip=');
 const apiKey = ('&appid=07679a828834f0056854e606b1ff9695&units=imperial');
 const zipcode = document.querySelector('#zip');
 const feelings = document.querySelector('#feelings');
-
+const temp = document.querySelector('#temp');
+const name = document.querySelector('#name');
 const button = document.querySelector('#generate');
-button.addEventListener('click', (event => {
-  event.preventDefault();
-  const newURL = `${baseURL}${zipcode.value}${apiKey}`
-  console.log(newURL);
-  fetchAPI(baseURL, apiKey);
-}))
+const feels_like = document.querySelector('#feels_like');
+const content = document.querySelector('#content');
+const weather = document.querySelector('#weather');
 
-const fetchAPI = async (baseURL, apiKey) => {
-  const res = await fetch(baseURL+ apiKey)
+button.addEventListener("click", (e) => {
+  e.preventDefault();
+  const madeURL = `${baseURL}${zip.value}${apiKey}`;
+  //fetch the url and get the data the needs to be sliced
+  fetchAPI(madeURL)
+    .then((data) => {
+      extractData(data)
+        .then((info) => {
+          postData('http://localhost:8000/add', info)
+        })
+    })
+});
+
+
+
+const fetchAPI = async (urlApi) => {
   try {
+    const res = await fetch(urlApi);
     const data = await res.json();
+    if (data.id != 0) {
+      console.log(data)
+      return data;
+
+    }
     console.log(data);
+    return data;
   } catch (error) {
-    console.error("error", error)
+    console.log(error.message);
   }
-}
+};
+
+const extractData = async (data) => {
+  try {
+    if (data.id != 0) {
+      console.log(data);
+      return data;
+    }
+
+    const content = {
+      date: newDate,
+      name: data.name,
+      temp: data.main.temp,
+      feels_like: data.main.feels_like,
+      content: feelings.value,
+      weather: data.weather[0].main
+    };
+    console.log(content);
+    return content;
+
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const postData = async (url = '', data = {}) => {
+  
+    const response = await fetch(url, {
+      method: 'POST',
+      credentials: "same-origin",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data),
+    }); 
+    const result = await response.json();
+    console.log(result);
+    return result;
+ 
+};
+
+
